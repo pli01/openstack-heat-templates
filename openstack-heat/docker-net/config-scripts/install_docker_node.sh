8fb843a7d0aa5e07a74cfa7fe0d6362fc2c9fdd1
pli01 <pli01@github.com>
2018-03-09 18:30:32 +0000

    Add install docker

--- openstack-heat/docker-net/config-scripts/install_docker_node.sh
+++ openstack-heat/docker-net/config-scripts/install_docker_node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "# RUNNING: $(dirname $0)/$(basename $0)"
-set -ex
+set -x
 # source config from heat
 config="$(dirname $0)/install_config_base.cfg"
 test -f "$config" || config=$config_file
@@ -8,6 +8,7 @@ test -f "$config" && . "$config"
 echo "# config $config"
 #
 
+export HOME=/root
 export DEBIAN_FRONTEND=noninteractive
 apt-get -qqy update
 apt-get install -qqy git ansible jq
@@ -15,10 +16,23 @@ update-ca-certificates --fresh --verbose
 
 export http_proxy
 export https_proxy
+export no_proxy
 (
 cd $(dirname $0)
 
+# get playbook
 git clone https://github.com/pli01/ansible-docker-host.git
 cd ansible-docker-host || exit 1
+
+# get roles
 bash -x build.sh
+
+# get custom environment config
+# TODO: use ansible extra-vars file -e @$ansible_env
+ansible_env=$ansible_config
+if [ -f "$ansible_env" ] ; then
+cp $ansible_env ansible/config/group_vars/docker/100-env
+fi
+
+bash -x deploy.sh
 )
