cbfbac285571240dea6054fdaa797aeb71fbea28
pli01 <pli01@github.com>
2018-03-30 12:47:46 +0000

    fix pipefail

--- tools/stack-is-ready.sh
+++ tools/stack-is-ready.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-set +x
+#set -x
 export stack_name=${1:? stack_name not defined}
 export openstack_cli=${openstack_cli:-openstack}
 
-set +o pipefail
-ret=false ; timeout=50 ; n=0
+#set +o pipefail
+ret=1 ; timeout=60 ; n=0
 echo "# $(basename $0): Check during ${timeout} seconds if ${stack_name} is ready  ?"
-while ! $ret ; do
-  ${openstack_cli} server list -c Name -f csv --quote=none --name ${stack_name} | awk ' NR > 1 { print $1 } ' | while read a ; do
-   ( set +o pipefail && ${openstack_cli} console log show $a 2>&1 |grep -q 'FINISH: INSTANCE CONFIGURED' ) && echo "$a ready" || echo "$a not ready"
-   done | grep "not ready" && ret=false || ret=true
-   echo "WAIT: $n $ret"
-   n=$(( n+1 ))
-   if [ $n -eq $timeout ] ;then  ret=true ; fi
+server_list=$(${openstack_cli} server list -c Name -f csv --quote=none --name ${stack_name} | awk ' NR > 1 { print $1 } ')
+
+until [ $n -ge $timeout ] || [ $ret -eq 0 ]
+do
+  set +o pipefail
+  for server in $server_list ; do
+   ( set +o pipefail && ${openstack_cli} console log show $server 2>&1 |grep -q 'FINISH: INSTANCE CONFIGURED' ) && echo "$server ready" || echo "$server not ready"
+  done | grep "not ready" && ret=1 || ret=0
+  echo "WAIT: $n $ret"
+  n=$(( n+1 ))
 done
