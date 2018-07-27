65251900d645b356038d88cb4a5b75d8e3bb280f
pli01 <pli01@github.com>
2018-07-27 15:21:38 +0000

    Add blue-green deployement script

---
+++ tools/blue-green-lib.sh
@@ -0,0 +1,167 @@
+#
+# common blue-green lib
+#
+function create_if_not_exist() {
+  eval $CREATE_IF_NOT_EXIST ; return $?
+}
+
+function log_error(){
+ echo "$@"
+ exit $1
+}
+
+function stack_is_present(){
+  [ -z "$1" ] && log_error 1 "argument stack absent"
+  stack_name=$1
+  ret=0; stack_status="" ; eval $(openstack $openstack_args stack show -c "stack_status" -f shell $stack_name ) ; [ -z "$stack_status" ] && ret=1 || echo "$stack_name $stack_status"
+  case "$stack_status" in
+    *COMPLETE) ret=0 ;;
+    *) ret=1 ;;
+  esac
+  return $ret
+}
+
+function get_fip_id(){
+  [ -z "$1" ] && log_error 1 "aucun argument"
+  stack_name=$1
+  ret=0
+  output_value="" ; eval $(openstack $openstack_args stack output show -c "output_value" -f shell $stack_name front_floating_ip_id ) ; [ -z "$stack_status" ] && ret=1 || echo "front_floating_ip_id=$output_value ; "
+  output_value="" ; eval $(openstack $openstack_args stack output show -c "output_value" -f shell $stack_name bastion_floating_ip_id ) ; [ -z "$stack_status" ] && ret=1 || echo "bastion_floating_ip_id=$output_value ;"
+  return $ret
+}
+
+function get_volume_id(){
+  [ -z "$1" ] && log_error 1 "aucun argument"
+  stack_name=$1
+  ret=0
+  output_value="" ; eval $(openstack $openstack_args stack output show -c "output_value" -f shell $stack_name data_volume_id ) ; [ -z "$stack_status" ] && ret=1 || echo "data_volume_id=$output_value ; "
+  return $ret
+}
+
+
+function stack_delete(){
+  $DRY_RUN openstack $openstack_args stack delete --yes --wait $1
+  return $?
+}
+
+function stack_create(){
+   # stack_create ${STACK_INFRA_NAME} ${STACK_DIR}/${STACK_INFRA_TEMPLATE} ${STACK_DIR}/${STACK_INFRA_PARAM} $plateforme $zone
+   stack_name=$1
+   stack_template=$2
+   stack_param=$3
+   plateforme=$4
+   zone=$5
+  [ -z "$stack_name" -o \
+    -z "$stack_template" -o \
+    -z "$stack_param" -o \
+    -z "$plateforme" -o \
+    -z "$zone" ] && log_error 1 "stack_create: argument manquant"
+  $DRY_RUN openstack $openstack_args stack create --wait -t $stack_template -e $stack_param $stack_name
+  return $?
+}
+
+function stack_update(){
+   stack_name=$1
+   stack_template=$2
+   stack_param=$3
+   plateforme=$4
+   zone=$5
+  [ -z "$stack_name" -o \
+    -z "$stack_template" -o \
+    -z "$stack_param" -o \
+    -z "$plateforme" -o \
+    -z "$zone" ] && log_error 1 "stack_update: argument manquant"
+   
+  $DRY_RUN openstack $openstack_args stack update --wait -t $stack_template -e $stack_param $stack_name
+  return $?
+}
+
+function stack_check(){
+   stack_name=$1
+  [ -z "$stack_name" ] && log_error 1 "stack_check: argument manquant"
+  $DRY_RUN openstack $openstack_args stack check --wait $stack_name
+  return $?
+}
+
+function get_param_stack_color(){
+  ret=1
+  case $1 in
+   blue) ret=0; 
+     echo "stack_fip=$STACK_FIP_BLUE_NAME;"
+     echo "stack_fip_template=$STACK_FIP_BLUE_TEMPLATE;"
+     echo "stack_fip_param=$STACK_FIP_BLUE_PARAM;"
+     echo "stack_volume=$STACK_VOLUME_BLUE_NAME;"
+     echo "stack_volume_template=$STACK_VOLUME_BLUE_TEMPLATE;"
+     echo "stack_volume_param=$STACK_VOLUME_BLUE_PARAM;"
+     echo "stack_infra=$STACK_INFRA_BLUE_NAME;"
+     echo "stack_infra_template=$STACK_INFRA_BLUE_TEMPLATE;"
+     echo "stack_infra_param=$STACK_INFRA_BLUE_PARAM;"
+   ;;
+   green) ret=0
+     echo "stack_fip=$STACK_FIP_GREEN_NAME;"
+     echo "stack_fip_template=$STACK_FIP_GREEN_TEMPLATE;"
+     echo "stack_fip_param=$STACK_FIP_GREEN_PARAM;"
+     echo "stack_volume=$STACK_VOLUME_GREEN_NAME;"
+     echo "stack_volume_template=$STACK_VOLUME_GREEN_TEMPLATE;"
+     echo "stack_volume_param=$STACK_VOLUME_GREEN_PARAM;"
+     echo "stack_infra=$STACK_INFRA_GREEN_NAME;"
+     echo "stack_infra_template=$STACK_INFRA_GREEN_TEMPLATE;"
+     echo "stack_infra_param=$STACK_INFRA_GREEN_PARAM;"
+   ;;
+   lb-blue) ret=0
+     echo "stack_fip=$STACK_FIP_LB_NAME;"
+     echo "stack_fip_template=$STACK_FIP_LB_TEMPLATE;"
+     echo "stack_fip_param=$STACK_FIP_LB_PARAM;"
+     echo "stack_infra=$STACK_LB_NAME;"
+     echo "stack_infra_template=$STACK_LB_TEMPLATE;"
+     echo "stack_infra_param=$STACK_LB_BLUE_PARAM;"
+   ;;
+   lb-green) ret=0
+     echo "stack_fip=$STACK_FIP_LB_NAME;"
+     echo "stack_fip_template=$STACK_FIP_LB_TEMPLATE;"
+     echo "stack_fip_param=$STACK_FIP_LB_PARAM;"
+     echo "stack_infra=$STACK_LB_NAME;"
+     echo "stack_infra_template=$STACK_LB_TEMPLATE;"
+     echo "stack_infra_param=$STACK_LB_GREEN_PARAM;"
+   ;;
+   lb) ret=0
+     echo "stack_fip=$STACK_FIP_LB_NAME;"
+     echo "stack_fip_template=$STACK_FIP_LB_TEMPLATE;"
+     echo "stack_fip_param=$STACK_FIP_LB_PARAM;"
+     echo "stack_infra=$STACK_LB_NAME;"
+     echo "stack_infra_template=$STACK_LB_TEMPLATE;"
+     echo "stack_infra_blue_param=$STACK_LB_BLUE_PARAM;"
+     echo "stack_infra_green_param=$STACK_LB_GREEN_PARAM;"
+   ;;
+
+   *) log_error 1 "ERROR: type inconnu" ; ret=1
+  esac
+  return $ret
+}
+
+function is_lb_last_stack_color_blue(){
+  echo "# is_lb_last_stack_color_blue $@"
+  stack_name=$1
+  ret=1
+  last_color=$(openstack $openstack_args stack show $stack_name -f json | jq -r .parameters.color)
+  echo "# Current lb color: $last_color"
+  [ "$last_color" == "blue" ] && ret=0
+  return $ret
+}
+
+function stack_create_lb(){
+  echo "# stack_create_lb $@"
+  next_stack_color=lb-$1
+  eval $(get_param_stack_color ${next_stack_color})
+  stack_create $stack_infra $STACK_DIR/$stack_infra_template $STACK_DIR/$stack_infra_param $plateforme $zone
+  return $?
+}
+
+function stack_update_lb(){
+  echo "# stack_update_lb $@"
+  next_stack_color=lb-$1
+  eval $(get_param_stack_color ${next_stack_color})
+  stack_check $stack_infra
+  stack_update $stack_infra $STACK_DIR/$stack_infra_template $STACK_DIR/$stack_infra_param $plateforme $zone
+  return $?
+}
---
+++ tools/blue-green.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+#
+# script de deploiement: blue/green
+#
+set -e
+
+# load lib
+[ -f $(dirname $0)/blue-green-lib.sh ] || exit 1
+source $(dirname $0)/blue-green-lib.sh
+
+plateforme=$1
+[ -z "$plateforme" ] && log_error 1 "ERROR: arg plateforme necessaire"
+
+
+openstack_args="${openstack_args:-} --insecure "
+STACK_DIR="heat-templates"
+DRY_RUN="${DRY_RUN:-echo }"
+CREATE_IF_NOT_EXIST="${CREATE_IF_NOT_EXIST:-false}"
+STACK_DELETE="${STACK_DELETE:-}"
+
+#
+# configuration
+## TODO: sortir la conf environnementale du script
+#
+## correspondance plateforme => zone
+case $plateforme in
+   dev) zone=z1 ; STACK_DELETE="${STACK_DELETE}" ;;
+   prod) zone=z3 ; STACK_DELETE="" ;;
+   *) echo "plateforme $plateforme inconnue"; exit 1 ;;
+esac
+
+APP=myapp
+## nom stack , template , parametre
+STACK_FIP_LB_NAME=floating-ip
+STACK_FIP_LB_TEMPLATE=floating-ip/heat.yaml
+STACK_FIP_LB_PARAM=floating-ip/param/heat-parameters_${zone}.yaml
+
+STACK_FIP_BLUE_NAME=floating-ip-blue
+STACK_FIP_BLUE_TEMPLATE=floating-ip/heat.yaml
+STACK_FIP_BLUE_PARAM=floating-ip/param/heat-parameters_${zone}.yaml
+
+STACK_FIP_GREEN_NAME=floating-ip-green
+STACK_FIP_GREEN_TEMPLATE=floating-ip/heat.yaml
+STACK_FIP_GREEN_PARAM=floating-ip/param/heat-parameters_${zone}.yaml
+
+STACK_VOLUME_BLUE_NAME=${APP}-data-blue
+STACK_VOLUME_BLUE_TEMPLATE=volumes/data_volume.yaml
+STACK_VOLUME_BLUE_PARAM=volumes/param/heat-parameters_${zone}.yaml
+
+STACK_VOLUME_GREEN_NAME=${APP}-data-green
+STACK_VOLUME_GREEN_TEMPLATE=volumes/data_volume.yaml
+STACK_VOLUME_GREEN_PARAM=volumes/param/heat-parameters_${zone}.yaml
+
+STACK_INFRA_BLUE_NAME=${APP}-blue
+STACK_INFRA_BLUE_TEMPLATE=infra/${APP}.yml
+STACK_INFRA_BLUE_PARAM=infra/param/${APP}-parameters_${zone}-blue.yaml
+
+STACK_INFRA_GREEN_NAME=${APP}-green
+STACK_INFRA_GREEN_TEMPLATE=infra/${APP}.yml
+STACK_INFRA_GREEN_PARAM=infra/param/${APP}-parameters_${zone}-green.yaml
+
+STACK_LB_NAME=${APP}-lb
+STACK_LB_TEMPLATE=lb/heat.yaml
+STACK_LB_BLUE_PARAM=lb/param/heat-parameters_${zone}-blue.yaml
+STACK_LB_GREEN_PARAM=lb/param/heat-parameters_${zone}-green.yaml
+
+#
+# check fip
+#
+for stack in lb blue green ; do
+echo "# Check $stack floating-ip stack"
+# get param
+eval $(get_param_stack_color ${stack})
+unset bastion_floating_ip_id
+unset front_floating_ip_id
+# check stack
+  if ! stack_is_present $stack_fip; then
+   if create_if_not_exist ;then 
+     echo "# Create $stack floating-ip stack"
+     stack_create $stack_fip $STACK_DIR/$stack_fip_template $STACK_DIR/$stack_fip_param $plateforme $zone
+   else
+     log_error "1" "ERROR: $stack_fip introuvable"
+     exit 1
+   fi
+  fi
+# get bastion fip id et front fip id
+## TODO: floatingip_id_bastion != bastion_floating_ip_id
+  echo "# Get ${stack_fip} bastion_floating_ip_id , front_floating_ip_id"
+  eval $(get_fip_id ${stack_fip})
+  echo "  bastion_floating_ip_id: $bastion_floating_ip_id"
+  echo "  front_floating_ip_id: $front_floating_ip_id"
+done
+
+#
+# volume
+#
+for stack in blue green ; do
+echo "# Check $stack volume stack"
+# get param
+eval $(get_param_stack_color ${stack})
+unset data_volume_id
+  if ! stack_is_present $stack_volume; then
+   if create_if_not_exist ;then 
+     echo "# Create $stack volume stack"
+     stack_create $stack_volume $STACK_DIR/$stack_volume_template $STACK_DIR/$stack_volume_param $plateforme $zone
+   else
+     log_error 1 "ERROR: Stack VOLUME ${stack_volume} absente"
+     exit 1
+   fi
+  fi
+  # get volume id
+  echo "# Get ${stack_volume} data_volume_id "
+  eval $(get_volume_id ${stack_volume})
+  echo "  data_volume_id: $data_volume_id"
+done
+
+#set -x
+# default and first time: blue
+echo "# Get last color"
+next_stack_color=blue
+# next time: get last state in metadata lb (blue or green)
+if stack_is_present $STACK_LB_NAME ;then
+  if is_lb_last_stack_color_blue $STACK_LB_NAME; then 
+    next_stack_color=green
+  else
+    next_stack_color=blue
+  fi
+fi
+echo "# Next color: ${next_stack_color}"
+
+eval $(get_param_stack_color ${next_stack_color})
+# create $next_stack_color
+if ! stack_is_present $stack_infra ; then
+#  stack_recreate $stack_infra
+  echo "# Create $stack_infra"
+  stack_create $stack_infra $STACK_DIR/$stack_infra_template $STACK_DIR/$stack_infra_param $plateforme $zone
+else
+  if [ ! -z "$STACK_DELETE" ] ; then
+    echo "# Recreate $stack_infra"
+    stack_delete $stack_infra
+    stack_create $stack_infra $STACK_DIR/$stack_infra_template $STACK_DIR/$stack_infra_param $plateforme $zone
+  else
+    echo "# Don't touch $stack_infra"
+  fi
+fi
+
+# update lb with $next_stack_color
+if ! stack_is_present $STACK_LB_NAME ; then
+  echo "# Create LB $next_stack_color"
+  stack_create_lb $next_stack_color
+else
+  echo "# Update LB $next_stack_color"
+  stack_update_lb $next_stack_color
+fi
+
+echo "# LB is $next_stack_color"
